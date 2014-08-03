0fdc70a072d08055c50e7a1e4c0a216054fc44d0
Iver Dihle Skjervum <iver.ds@gmail.com>
2014-08-04 00:18:26 +0200

    Lagt til visning med bakgrunnsbilde

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Kine og Iver' });
 });
 
+router.get('/background', function(req, res) {
+  res.render('background', { title: 'Kine og Iver' });
+});
+
 module.exports = router;
