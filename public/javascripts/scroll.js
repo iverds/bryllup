cefabfcb73f0b6901d05209d6c0d346665f6c8d3
Iver Dihle Skjervum <iver.ds@gmail.com>
2014-11-06 01:18:08 +0100

    Masse masse responsiv-greier

--- public/javascripts/scroll.js
+++ public/javascripts/scroll.js
@@ -1,9 +1,35 @@
 $(function() {
     var scrolling = false;
+    var lastScroll = 0;
     var menu = {"main" : {"number" : 0}, "friday": {"number" : 1}, "saturday": {"number" : 2}, "sunday": { "number" : 3}}
     $(window).bind('mousewheel', function(event, delta) {
+      /*if(scrolling) {
+        return false;
+      }*/
       if(event.originalEvent.wheelDelta <= 0 ) {  scrollNext();  }
       else { scrollPrev(); }
+
+      /*console.log($(".slide.friday").offset().top.toFixed());
+      console.log($("body").scrollTop());
+      console.log($(".slide.friday").offset().top.toFixed() == $("body").scrollTop())
+      if(lastScroll < $(".slide.friday").offset().top.toFixed() && $("body").scrollTop() >= $(".slide.friday").offset().top.toFixed()) {
+        console.log("PASSED!");
+        var nextSlide = $(".active").nextAll(".slide:first")
+        scrolling = true;
+        $(".menu-circle").css("opacity", "0.5")
+        var currentSlide = $(".active")
+        $(".menu-" + nextSlide.data("slide")).css("opacity", "1");
+        $("html, body").animate({
+          scrollTop: nextSlide.offset().top
+          //top: calculatePosition(nextSlide),
+        }, 2000, function() {
+          nextSlide.addClass("active")
+          currentSlide.removeClass("active")
+          scrolling = false;
+        });
+      }
+
+      lastScroll = $("body").scrollTop();*/
       return false;
     });
 
@@ -39,7 +65,8 @@ $(function() {
         var currentSlide = $(".active")
         $(".menu-" + nextSlide.data("slide")).css("opacity", "1");
         $(".wrapper").animate({
-          top: calculatePosition(nextSlide),
+          //scrollTop: nextSlide.offset().top
+          top: calculatePosition(nextSlide)
         }, 2000, function() {
           nextSlide.addClass("active")
           currentSlide.removeClass("active")
