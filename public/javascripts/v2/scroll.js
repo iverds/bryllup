d9550006173dc7dcdaaf0b3f8d8eaa65569aed66
Iver Dihle Skjervum <iver.ds@gmail.com>
2014-11-06 23:39:13 +0100

    Fikset meny

--- public/javascripts/v2/scroll.js
+++ public/javascripts/v2/scroll.js
@@ -1,52 +1,30 @@
 $(function() {
     var scrolling = false;
-    var lastScroll = 0;
-    var menu = {"main" : {"number" : 0}, "friday": {"number" : 1}, "saturday": {"number" : 2}, "sunday": { "number" : 3}}
-
     $(".menu-link").on("click", function(e) {
       e.preventDefault();
       scroll($("." + $(e.currentTarget).data("menu-button")));
       return false;
     })
 
-
-    $(document).on('keypress', scrollKeys);
-    $(document).on('keydown', scrollKeys);
-
-
-    function scrollKeys(e) {
-      if(e.keyCode == 40) {
-        e.preventDefault();
-        scrollNext();
-      } else if (e.keyCode == 38) {
-        e.preventDefault();
-        scrollPrev();
-      }
-    }
-
-
+    $(window).bind('mousewheel', function(event, delta) {
+      $.each($(".slide"), function(k,v) {
+        if($("body").scrollTop() > $(v).offset().top - $(v).height()/2 && $("body").scrollTop() < $(v).offset().top + $(v).height()/2) {
+          $(".menu-link").css("opacity", "0.5");
+          $(".menu-" + $(v).data("slide")).css("opacity", "1");
+        }
+      });
+    });
 
     function scroll(nextSlide) {
       if(!scrolling && nextSlide.length !== 0) {
         scrolling = true;
+        $(".menu-link").css("opacity", "0.5");
+        $(".menu-" + nextSlide.data("slide")).css("opacity", "1");
         $("html, body").animate({
-          scrollTop: nextSlide.offset().top - 60
+          scrollTop: nextSlide.offset().top
         }, 2000, function() {
           scrolling = false;
         });
       }
-    }
-
-    function calculatePosition(slide) {
-      var slideInfo = menu[slide.data("slide")]
-      return "-" + (slideInfo.number * 100 + slideInfo.number *2) + "%"
-    }
-
-    function scrollNext() {
-      scroll($(".active").nextAll(".slide:first"));
-    }
-
-    function scrollPrev() {
-      scroll($(".active").prevAll(".slide:first"));
     }
   });
