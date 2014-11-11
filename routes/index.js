2a5e0acc3c742409278a6b9a332e216eb6b94bc8
Iver Dihle Skjervum <iver.ds@gmail.com>
2014-11-11 22:10:59 +0100

    Updates updates

--- routes/index.js
+++ routes/index.js
@@ -6,18 +6,4 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Kine og Iver' });
 });
 
-router.get('/background', function(req, res) {
-  res.render('background', { title: 'Kine og Iver', user: req.user });
-});
-
-router.get('/background-blur', function(req, res) {
-  res.render('background-blur', { title: 'Kine og Iver' });
-});
-
-router.get('/background-opacity', function(req, res) {
-  res.render('background-opacity', { title: 'Kine og Iver' });
-});
-
-
-
 module.exports = router;
